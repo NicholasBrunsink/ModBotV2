341d2fea2264b9e21cbecc6ab571bc14e04fd948
NicholasBrunsink <nicholasbrunsink@gmail.com>
2024-11-27 11:00:15 -0500

    yo

--- app.py
+++ app.py
@@ -18,9 +18,17 @@ def webhook():
     return "ok", 200
 
 def checkMsg(data):
+    # opening the file in read mode 
+    my_file = open("bannedwords.txt", "r") 
+    # reading the file 
+    words = my_file.read() 
+    # replacing end of line('/n') with ' ' and 
+    # splitting the text it further when '.' is seen. 
+    wordList = data.replace('\n', ' ').split(".") 
+    print(wordList)
 
-    url  = f'https://api.groupme.com/v3/bots/groups/{data["group_id"]}/members/{data["membership_id"]}/remove?token={os.getenv('ACCESS_TOKEN')}'
-    print(url)
+    # url  = f'https://api.groupme.com/v3/bots/groups/{data["group_id"]}/members/{data["membership_id"]}/remove?token={os.getenv('ACCESS_TOKEN')}'
+    # print(url)
 
     # $ curl -X POST -H https://api.groupme.com/v3/groups?token=YOUR_ACCESS_TOKEN
     
