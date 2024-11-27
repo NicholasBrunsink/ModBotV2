8676d135c29e97db3f6f78c60b48417ca968ab2e
NicholasBrunsink <nicholasbrunsink@gmail.com>
2024-11-27 12:20:48 -0500

    test 18

--- app.py
+++ app.py
@@ -25,11 +25,21 @@ def checkMsg(data):
     print(data["text"])
     if words_re.search(data["text"].lower()):
         group = data["group_id"]
-        id = data["id"]
+        userId = data["sender_id"]
         token = os.getenv("ACCESS_TOKEN")
+        getUrl="https://api.groupme.com/v3/groups/{group}?token={token}"
+        resp=requests.get(getUrl)
+
+        id=""
+        for member in resp["members"]:
+            if member["user_id"] == userId:
+                id = member["id"]
+                break
+        print(id)
+
         url  = f'https://api.groupme.com/v3/bots/groups/{group}/members/{id}/remove?token={token}'
         print(url)
-        requests.post(url)
+        # print(requests.post(url))
     infile.close()
 
     return
