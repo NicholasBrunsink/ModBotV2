88bec5dc5ae53d77228a36d16064593b9fed95ef
NicholasBrunsink <nicholasbrunsink@gmail.com>
2024-11-27 13:26:09 -0500

    final?

--- app.py
+++ app.py
@@ -17,40 +17,40 @@ def webhook():
     return "ok", 200
 
 def checkMsg(data):
-    infile = open("bannedwords.txt", "r") 
-    safefile = open("safepeople.txt", "r")
-    safe = safefile.read().replace('\n', " ").split(" ")
-    words = infile.read().replace('\n', ' ').split(" ") 
-    infile.close()
-    safefile.close()
+    if data["sender_type"] == "user":
+        infile = open("bannedwords.txt", "r") 
+        safefile = open("safepeople.txt", "r")
+        safe = safefile.read().replace('\n', " ").split(" ")
+        words = infile.read().replace('\n', ' ').split(" ") 
+        infile.close()
+        safefile.close()
 
-    words_re = re.compile("|".join(words))
+        words_re = re.compile("|".join(words))
 
-    if words_re.search(data["text"].lower()):
-        print(data)
-        group = data["group_id"]
-        userId = data["sender_id"]
-        token = os.getenv("ACCESS_TOKEN")
+        if words_re.search(data["text"].lower()):
+            group = data["group_id"]
+            userId = data["sender_id"]
+            token = os.getenv("ACCESS_TOKEN")
 
-        getUrl=f"https://api.groupme.com/v3/groups/{group}?token={token}"
-        resp=requests.get(getUrl).json()
-        if resp["meta"]["code"] != 200:
-            return
-        members = resp["response"]
-        id=""
-        for member in members["members"]:
-            if member["user_id"] == userId and member["id"] not in safe:
-                id = member["id"]
-                name = member["name"]
-                print(f"kicking {id}: {name}")
-                break
+            getUrl=f"https://api.groupme.com/v3/groups/{group}?token={token}"
+            resp=requests.get(getUrl).json()
+            if resp["meta"]["code"] != 200:
+                return
+            members = resp["response"]
+            id=""
+            for member in members["members"]:
+                if member["user_id"] == userId and member["id"] not in safe:
+                    id = member["id"]
+                    name = member["name"]
+                    print(f"kicking {id}: {name}")
+                    break
 
-        if id == "":
-            print("not kicking (no user or protected user)")
-            return
+            if id == "":
+                print("not kicking (no user or protected user)")
+                return
 
-        url  = f'https://api.groupme.com/v3/groups/{group}/members/{id}/remove?token={token}'
-        requests.post(url)
-        print(f"Kicked {id} ({name}) from {group}")
+            url  = f'https://api.groupme.com/v3/groups/{group}/members/{id}/remove?token={token}'
+            requests.post(url)
+            print(f"Kicked {id} ({name}) from {group}")
 
     return
