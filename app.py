7ed24d331acb70469603c226aa1a54eb91204e7a
NicholasBrunsink <nicholasbrunsink@gmail.com>
2024-12-21 20:14:02 -0500

    added documentation

--- app.py
+++ app.py
@@ -8,56 +8,61 @@ from urllib.request import Request, urlopen
 
 from flask import Flask, request
 
+# Creates list of banned words and safe users. Runs when the app is first created
 infile = open("bannedwords.txt", "r") 
 safefile = open("safepeople.txt", "r")
 safe = safefile.read().replace("\n", "<>").split(">")
-safe = [x.split(":")[0] if x[0]!=":" else "<" for x in safe]
-safe.remove("<")
+safe = [x.split(":")[0] if x[0]!=":" else "<" for x in safe].remove("<")
 words = infile.read().replace('\n', ' ').split(" ") 
 infile.close()
 safefile.close()
+words_re = re.compile("|".join(words))
 
 app = Flask(__name__)
 
+# is called by GroupMe API. If the message is sent by a user, it will call checkMsg
 @app.route('/', methods=['POST'])
 def webhook():
     data = request.get_json()
-    checkMsg(data)
+    if data["sender_type"] == "user" and data["sender_id"] not in safe:
+        checkMsg(data)
+    else:
+        print("Protected User and/or system message")
+
     return "ok", 200
 
+"""
+    Checks incoming message for any banned words
+    Doesn't kick if a user is part of the protected user's list
+""" 
 def checkMsg(data):
-    if data["sender_type"] == "user":
-        words_re = re.compile("|".join(words))
+    if words_re.search(data["text"].lower()):
+        group = data["group_id"]
+        userId = data["sender_id"]
+        token = os.getenv("ACCESS_TOKEN")
 
-        if words_re.search(data["text"].lower()):
-            group = data["group_id"]
-            userId = data["sender_id"]
-            token = os.getenv("ACCESS_TOKEN")
+        # get source group's members list
+        getUrl=f"https://api.groupme.com/v3/groups/{group}?token={token}"
+        resp=requests.get(getUrl).json()
+        if resp["meta"]["code"] != 200:
+            return
+        
+        # searches members list to find user's group_id of sent message
+        # Also grabs their name for logging purposes
+        members = resp["response"]
+        id=""; name=""
+        for member in members["members"]:
+            if member["user_id"] == userId:
+                name = member["name"]
+                id = member["id"]
+                break
 
-            getUrl=f"https://api.groupme.com/v3/groups/{group}?token={token}"
-            resp=requests.get(getUrl).json()
-            if resp["meta"]["code"] != 200:
-                return
-            members = resp["response"]
-            id=""; name=""
-            for member in members["members"]:
-                if member["user_id"] == userId:
-                    name = member["name"]
-                    if member["user_id"] not in safe:
-                        id = member["id"]
-                        print(f"kicking {id}: {name}")
-                    break
+        # Logging statements
+        if id == "":
+            print("not kicking ", end="-")
+            if name=="":
+                print("no user")
+            return
 
-            if id == "":
-                print("not kicking ", end="-")
-                if name=="":
-                    print("no user")
-                else:
-                    print("protected user")
-                return
-
-            url  = f'https://api.groupme.com/v3/groups/{group}/members/{id}/remove?token={token}'
-            requests.post(url)
-            print(f"Kicked {id} ({name}) from {group}")
-
-    return
+        requests.post(f'https://api.groupme.com/v3/groups/{group}/members/{id}/remove?token={token}')
+        print(f"Kicked {id} ({name}) from {group}")
